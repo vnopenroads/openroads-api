89ad9a3d0792b09618a5f466d5eb5ddf7b1395c0
Derek Lieu <dl@dereklieu.com>
2015-02-27 15:36:56 -0500

    Model attributes for Nodes

--- api/models/Node.js
+++ api/models/Node.js
@@ -1,14 +1,48 @@
 /**
 * Node.js
 *
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @description :: Represents nodes.
+* For schema, see: http://chrisnatali.github.io/osm_notes/osm_schema.html#current_nodes
+*
+*
+*
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
 
 module.exports = {
 
   attributes: {
+    id: {
+      type: 'integer',
+      autoIncrement: true,
+      unique: true,
+      primaryKey: true,
+    },
+    latitude: 'float'
+    longitude: 'float',
+    changeset_id: 'integer',
+    visible: {
+      type: 'boolean',
+    },
+    timestamp: {
+      type: 'datetime',
+    },
 
+    // OSM uses tile # to narrow down bbox queries.
+    // No plans to implement that at the moment, since we have far fewer nodes.
+    // TODO make sure this doesn't bog down for our db size.
+    tile: {
+      type: 'integer',
+      unique: true,
+    },
+    version: {
+      type: 'integer',
+    },
+
+    // Foreign keys
+    changeset_id_fkey: {
+      model: 'changeset'
+    },
   }
 };
 
