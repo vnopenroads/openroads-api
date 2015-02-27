960a6c4cb774b405a8935e2033e6fa6e4d308ec2
Derek Lieu <dl@dereklieu.com>
2015-02-27 15:15:10 -0500

    Renaming to ways, nodes, waynodes. Placeholder files

---
+++ api/controllers/NodeController.js
@@ -0,0 +1,11 @@
+/**
+ * NodeController
+ *
+ * @description :: Server-side logic for managing nodes
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
