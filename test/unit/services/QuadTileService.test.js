9b07366b76fe853e3b6615ebe8281f70b1182256
Derek Lieu <dl@dereklieu.com>
2015-03-02 14:39:31 -0500

    2 space indents

--- test/unit/services/QuadTileService.test.js
+++ test/unit/services/QuadTileService.test.js
@@ -1,14 +1,14 @@
 var geoRecordScale = 10000000;
 
 describe('QuadTileService', function() {
-    describe('#lonlat2tile', function() {
-        it('Should return the correct tile for a lat/lon pair', function() {
-            var latitude = 97378629 / geoRecordScale;
-            var longitude = 1237890525 / geoRecordScale; 
-            QuadTileService.xy2tile(
-                QuadTileService.lon2x(longitude),
-                QuadTileService.lat2y(latitude)
-            ).should.equal(3805368681)
-        })
+  describe('#lonlat2tile', function() {
+    it('Should return the correct tile for a lat/lon pair', function() {
+      var latitude = 97378629 / geoRecordScale;
+      var longitude = 1237890525 / geoRecordScale;
+      QuadTileService.xy2tile(
+        QuadTileService.lon2x(longitude),
+        QuadTileService.lat2y(latitude)
+      ).should.equal(3805368681)
     })
+  })
 })
