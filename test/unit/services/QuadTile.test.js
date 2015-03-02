6067a4188dae2899794f2cb68f780d417e0415b3
Marc Farra <marcfarra@gmail.com>
2015-03-02 17:51:26 -0500

    rename test

--- test/unit/services/QuadTile.test.js
+++ test/unit/services/QuadTile.test.js
@@ -1,13 +1,13 @@
 var geoRecordScale = 10000000;
 
-describe('QuadTileService', function() {
+describe('QuadTile', function() {
   describe('#lonlat2tile', function() {
     it('Should return the correct tile for a lat/lon pair', function() {
       var latitude = 97378629 / geoRecordScale;
       var longitude = 1237890525 / geoRecordScale;
-      QuadTileService.xy2tile(
-        QuadTileService.lon2x(longitude),
-        QuadTileService.lat2y(latitude)
+      QuadTile.xy2tile(
+        QuadTile.lon2x(longitude),
+        QuadTile.lat2y(latitude)
       ).should.equal(3805368681)
     })
   })
