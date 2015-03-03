26988c1a4088b4ed2ecd190c07fb91731f6a09b0
Derek Lieu <dl@dereklieu.com>
2015-03-03 18:22:46 -0500

    MapController gathers missing nodes

--- api/controllers/MapController.js
+++ api/controllers/MapController.js
@@ -33,38 +33,73 @@ module.exports = {
       else {
 
         // Create a list of node ID's.
-        var nodeIDs = _.map(nodes, function(node) { return node.node_id; });
+        var nodeIDs = _.pluck(nodes, 'node_id');
 
         // Query the way_nodes endpoint, returning every wayNode containing our nodes.
         Way_Nodes.find({ node_id: nodeIDs }).exec(function wayNodeResp(err, wayNodes) {
 
-          // Create a list of way ID's.
-          var wayIDs = _.map(wayNodes, function(wayNode) { return wayNode.way_id; });
+          // Get a unique list of way ID's
+          var wayIDs = _(wayNodes).pluck('way_id').uniq().value();
 
-          // Query the ways endpoint with a unique list of way ID's.
-          ways = Ways.find({ way_id: _.uniq(wayIDs) }).exec(function wayResp(err, ways) {
+          // Perform two separate db queries now using our unique way ID's.
+          async.parallel({
 
-            // For each way, attach every node it contains using the wayNodes server response.
-            for (var j = 0, jj = ways.length; j < jj; ++j) {
-              var way = ways[j];
-              var nodesInWay = [];
-              for (var i = 0, ii = wayNodes.length; i < ii; ++i) {
-                var wayNode = wayNodes[i];
-                if (wayNode.way_id === way.way_id) {
-                  nodesInWay.push(wayNode);
-                }
-              }
-              way.nodes = nodesInWay;
+            // First query returns the actual ways.
+            ways: function(cb) {
+              Ways.find({ way_id: wayIDs }).exec(cb);
+            },
+
+            // Second query hits way_nodes table again
+            // to get any *nodes* that aren't in our BBox,
+            // but are part of a way.
+            wayNodes: function(cb) {
+              Way_Nodes.find({ way_id: wayIDs }).exec(cb);
             }
 
-            res.set('Content-Type', 'text/xml');
-            var xmlDoc = XML.write({bbox: bbox, nodes: nodes, ways: ways});
-            return res.send(xmlDoc.toString());
+          }, function wayResp(err, resp) {
+
+            var allNodeIDs = _(resp.wayNodes).pluck('node_id').uniq().value();
+            var missingNodes = _.difference(allNodeIDs, nodeIDs);
+            var ways = attachNodeIDs(resp);
+            var xmlDoc;
+
+            // Need to hit the Nodes server again.
+            if (missingNodes.length) {
+              Nodes.find({ node_id: missingNodes }).exec(function missingNodeResp(err, missingNodes) {
+                nodes = nodes.concat(missingNodes);
+                xmlDoc = XML.write({bbox: bbox, nodes: nodes, ways: ways});
+                res.set('Content-Type', 'text/xml');
+                return res.send(xmlDoc.toString());
+              });
+            }
+
+            // We have all the nodes, write the output.
+            else {
+              xmlDoc = XML.write({bbox: bbox, nodes: nodes, ways: ways});
+              res.set('Content-Type', 'text/xml');
+              return res.send(xmlDoc.toString());
+            }
           });
         });
-
       }
-
     });
   }
 };
+
+function attachNodeIDs(obj) {
+  var ways = obj.ways;
+  var wayNodes = obj.wayNodes;
+  // For each way, attach every node it contains using the wayNodes server response.
+  for (var j = 0, jj = ways.length; j < jj; ++j) {
+    var way = ways[j];
+    var nodesInWay = [];
+    for (var i = 0, ii = wayNodes.length; i < ii; ++i) {
+      var wayNode = wayNodes[i];
+      if (wayNode.way_id === way.way_id) {
+        nodesInWay.push(wayNode);
+      }
+    }
+    way.nodes = nodesInWay;
+  }
+  return ways;
+}
