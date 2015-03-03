3dff0737d7d99012f97e6458ed8368ecd9f0560b
Derek Lieu <dl@dereklieu.com>
2015-03-03 15:28:50 -0500

    XML writer returns ways xml

--- api/services/XML.js
+++ api/services/XML.js
@@ -22,9 +22,9 @@ module.exports = {
     }
 
     if (nodes) {
-      for(var i = 0, ii = nodes.length; i < ii; ++i) {
+      for (var i = 0, ii = nodes.length; i < ii; ++i) {
         var node = nodes[i];
-        var attr = {
+        root.node('node').attr({
           id: node.node_id,
           visible: node.visible,
           version: node.version,
@@ -34,8 +34,38 @@ module.exports = {
           uid: 1,
           lat: node.latitude / info.ratio,
           lon: node.longitude / info.ratio
-        };
-        root.node('node').attr(attr);
+        });
+      }
+    }
+
+    if (ways) {
+      for (var i = 0, ii = ways.length; i < ii; ++i) {
+        var way = ways[i];
+        var wayEl = root.node('way').attr({
+          id: way.way_id,
+          visible: way.visible,
+          version: way.version,
+          changeset: way.changeset_id,
+          timestamp: way.timestamp,
+          user: 'DevelopmentSeed',
+          uid: 1
+        });
+
+        // Use the sequence ID to make sure nodes are ordered correctly.
+        var wayNodes = way.nodes;
+        var orderedNodes = [];
+        for (var j = 0, jj = wayNodes.length; j < jj; ++j) {
+          var wayNode = wayNodes[j];
+          orderedNodes[parseInt(wayNode.sequence_id, 10)] = wayNode.node_id;
+        }
+
+        // Attach a node ref for each node, as long as it exists and it's id isn't '0'.
+        for(var k = 0, kk = orderedNodes.length; k < kk; ++k) {
+          var wayNode = orderedNodes[k];
+          if (wayNode && wayNode !== '0') {
+            wayEl.node('nd').attr({ ref: wayNode });
+          }
+        }
       }
     }
 
