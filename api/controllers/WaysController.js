9f893809be64cabcca5dbdccc90cb1931d036938
Derek Lieu <dl@dereklieu.com>
2015-03-10 14:28:55 -0400

    full ways endpoint in ways controller

--- api/controllers/WaysController.js
+++ api/controllers/WaysController.js
@@ -6,6 +6,41 @@
  */
 
 module.exports = {
-	
+
+  full: function(req, res) {
+    var wayId = req.param('way_id');
+
+    // ill-formed way id
+    if (!wayId || isNaN(wayId)) {
+      return res.badRequest('Way ID must be a non-zero number');
+    }
+
+    // Query both the way and it's associated nodes
+    async.parallel({
+      ways: function(cb) {
+        Ways.find({ way_id: wayId }).exec(cb);
+      },
+      wayNodes: function(cb) {
+        Way_Nodes.find({ way_id: wayId }).exec(cb);
+      }
+    }, function wayResp(err, resp) {
+      var ways = resp.ways.length ? Ways.attachNodeIDs(resp.ways, resp.wayNodes) : [{}];
+      var nodeIDs = _(resp.wayNodes).pluck('node_id').uniq().value();
+
+      // If the way is not found, or isn't visible
+      if (!ways[0].visible) {
+        res.set('Content-Type', 'text/xml');
+        return res.send('<osm version="' + Api.version + '" generator="'
+                        + Api.generator + '"></osm>');
+      }
+
+      // All ways must have nodes, so don't bother checking wayNodes.length
+      Nodes.find({ node_id: nodeIDs }).exec(function nodeResp(err, nodes) {
+        var xmlDoc = XML.write ({ nodes: nodes, ways: ways });
+        res.set('Content-Type', 'text/xml');
+        return res.send(xmlDoc.toString());
+      });
+    });
+  },
 };
 
