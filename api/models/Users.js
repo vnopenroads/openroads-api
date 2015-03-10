f9682c34a3b99db253e03818c4b2d94aa7fde753
Derek Lieu <dl@dereklieu.com>
2015-03-10 17:29:22 -0400

    Placeholder user model, controller

---
+++ api/models/Users.js
@@ -0,0 +1,18 @@
+/**
+* Users.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+  attributes: {
+    id: {
+      type: 'integer',
+      autoIncrement: true,
+      unique: true,
+      primaryKey: true,
+      numerical: true
+    },
+  }
+};
