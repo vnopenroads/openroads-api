9120963ae3c24fe7df35c3a5e2db937f786ed3aa
Derek Lieu <dl@dereklieu.com>
2015-03-10 18:32:58 -0400

    Basic attributes to user model

--- api/models/Users.js
+++ api/models/Users.js
@@ -9,10 +9,20 @@ module.exports = {
   attributes: {
     id: {
       type: 'integer',
-      autoIncrement: true,
       unique: true,
       primaryKey: true,
-      numerical: true
+      numerical: true,
+      index: true,
     },
+    display_name: {
+      type: 'string',
+      required: true
+    },
+    creation_time: {
+      type: 'datetime',
+      datetime: true,
+      required: true
+    }
   }
 };
+
