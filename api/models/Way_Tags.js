2b8f657e135ee4c8a62f7c612f8b380be104c497
Derek Lieu <dl@dereklieu.com>
2015-03-12 13:32:26 -0400

    Old way tags, old node tags

--- api/models/Way_Tags.js
+++ api/models/Way_Tags.js
@@ -1,32 +1,29 @@
 /**
-* Way_tags.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * Way_tags.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
 
 module.exports = {
 
+  tableName: 'current_way_tags',
+
   attributes: {
     way_id: {
-        type: 'integer',
-        primaryKey: true,
-        autoIncrement: true,
-        model: 'ways'
+      type: 'integer',
+      primaryKey: true,
+      autoIncrement: true,
+      model: 'ways'
     },
     k: {
-        type: 'string',
-        truthy: true
+      type: 'string',
+      truthy: true
     },
     v: {
-        type: 'string',
-        truthy: true
+      type: 'string',
+      truthy: true
     },
-    version: {
-        type: 'integer',
-        numerical: true
-    },
-
   },
 
   //Translate the entity from the XML parser into a proper model
