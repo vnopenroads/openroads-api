f46d59bed6b950d573f6bc7f83d66a0fa6194e85
Derek Lieu <dl@dereklieu.com>
2015-03-18 18:15:10 -0400

    Tests for xml changereader

--- test/unit/services/XML.test.js
+++ test/unit/services/XML.test.js
@@ -1,99 +1,5 @@
-var xmlNodeModify = '<osmChange version="0.3" generator="iD">' +
-  '<create/>' +
-  '<modify>' +
-    '<node id="-76703" lon="123.83676223498107" lat="9.632539331799256" version="1" changeset="123"/>' +
-  '</modify>'  +
-  '<delete if-unused="true"/>' +
-'</osmChange>'
-
-var jsonNodeModify = [
-  { action: 'modify',
-    model: 'node',
-    id: -76703,
-    indexName: 'id',
-    attributes:
-     { latitude: 96325393,
-       longitude: 1238367622,
-       changeset_id: 123,
-       visible: true,
-       tile: 3805365679,
-       version: 1,
-       timestamp: new Date() }  } ];
-
-var xmlCreateWay = '<osmChange version="0.3" generator="iD">' +
-  '<create>' +
-    '<node id="-7" lon="123.83621506434201" lat="9.632200849325827" version="0" changeset="123"/>' +
-    '<node id="-10" lon="123.84027056437253" lat="9.63569143353531" version="0" changeset="123"/>' +
-    '<way id="-4" version="0" changeset="123">' +
-      '<nd ref="-7"/>' +
-      '<nd ref="-10"/>' +
-      '<tag k="highway" v="residential"/>' +
-      '<tag k="name" v="Fake Street"/>' +
-    '</way>' +
-  '</create>' +
-  '<modify/>' +
-  '<delete if-unused="true"/>' +
-'</osmChange>';
-
-var jsonWayCreate = [
-  { action: 'create',
-    model: 'node',
-    id: -7,
-    indexName: 'id',
-    attributes:
-     { latitude: 96322008,
-       longitude: 1238362150,
-       changeset_id: 123,
-       visible: true,
-       tile: 3805365678,
-       version: 1,
-       timestamp: new Date() } },
-  { action: 'create',
-    model: 'node',
-    id: -10,
-    indexName: 'id',
-    attributes:
-     { latitude: 96356914,
-       longitude: 1238402705,
-       changeset_id: 123,
-       visible: true,
-       tile: 3805366032,
-       version: 1,
-       timestamp: new Date() } },
-  { action: 'create',
-    model: 'way',
-    id: -4,
-    indexName: 'id',
-    attributes:
-     {
-       changeset_id: 123,
-       timestamp: new Date(),
-       version: 1,
-       visible: true } },
-  { action: 'create',
-    model: 'way_node',
-    id: -7,
-    indexName: 'node_id',
-    attributes: { way_id: -4, node_id: -7, sequence_id: 0 } },
-  { action: 'create',
-    model: 'way_node',
-    id: -10,
-    indexName: 'node_id',
-    attributes: { way_id: -4, node_id: -10, sequence_id: 1 } },
-  { action: 'create',
-    model: 'way_tag',
-    id: -4,
-    indexName: 'way_id',
-    attributes: { way_id: -4, k: 'highway', v: 'residential' } },
-  { action: 'create',
-    model: 'way_tag',
-    id: -4,
-    indexName: 'way_id',
-    attributes: {
-      way_id: -4,
-      k: 'name',
-      v: 'Fake Street'
-    } } ];
+var libxml = require('libxmljs');
+var mock = require('../helpers/XML.readChanges');
 
 function rmTimestamps(actionArray) {
   return actionArray.map(function(action) {
@@ -110,12 +16,35 @@ function rmTimestamps(actionArray) {
 describe('XML', function() {
   describe('#readChanges', function() {
     it('Should translate a single node modify', function() {
-      rmTimestamps(XML.readChanges(xmlNodeModify))
-        .should.be.eql(rmTimestamps(jsonNodeModify))
+      rmTimestamps(XML.readChanges(mock.xmlNodeModify))
+        .should.be.eql(rmTimestamps(mock.jsonNodeModify))
     }),
     it('Should translate a way creation', function() {
-      rmTimestamps(XML.readChanges(xmlCreateWay))
-        .should.be.eql(rmTimestamps(jsonWayCreate))
+      rmTimestamps(XML.readChanges(mock.xmlCreateWay))
+        .should.be.eql(rmTimestamps(mock.jsonCreateWay))
     })
-  })
-})
+  });
+
+  describe('#write', function() {
+    it('Should correctly write tags to nodes', function() {
+      var nodes = [{
+        tags: [{ k: 1, v: 1 }]
+      }];
+      var xml = XML.write({ nodes: nodes }).toString();
+      var doc = libxml.parseXmlString(xml);
+      var tag = doc.find('//tag');
+      doc.get('//tag').toString().should.eql('<tag k="1" v="1"/>');
+    });
+    it('Should correctly write tags to ways', function() {
+      var ways = [{
+        tags: [{ k: 1, v: 1 }],
+        nodes: []
+      }];
+      var xml = XML.write({ ways: ways }).toString();
+      var doc = libxml.parseXmlString(xml);
+      var tag = doc.find('//tag');
+      doc.get('//tag').toString().should.eql('<tag k="1" v="1"/>');
+    });
+  });
+
+});
