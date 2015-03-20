b053690aa2a65567e9524fe4d3118f2f77364beb
Marc Farra <marcfarra@gmail.com>
2015-03-20 18:16:37 -0400

    hotfix #130

--- api/controllers/ChangesetsController.js
+++ api/controllers/ChangesetsController.js
@@ -189,6 +189,8 @@ module.exports = {
 
           } else if (action.action === 'modify') {
             // Save the original entity in the old entity (nodes, ways, etc) tables.
+            // Update version of the model
+            action.attributes.version += 1;
             var oldEntity = _.clone(action.attributes, true);
 
             // Assign ID to oldEntity
@@ -200,8 +202,6 @@ module.exports = {
             .insert(oldEntity)
             .then(function() {
 
-              // Update version of the model and its attributes in the current_ tables
-              action.attributes.version += 1;
               return transaction(currentTable)
               .where(action.indexName, '=', action.id)
               .update(action.attributes)
