445d0d2663aa4b11a00d5688047c44c81b6883b4
Marc Farra <marcfarra@gmail.com>
2015-04-02 18:23:34 -0400

    Add boom error handling

--- routes/changesetUpload.js
+++ routes/changesetUpload.js
@@ -1,11 +1,29 @@
-var knex = require('knex');
+var _ = require('lodash')
+var Boom = require('boom')
+var knex = require('knex')({
+  client: 'pg',
+  connection: require('../local').connections.osmPostgreSQL.url,
+  debug: true
+});
+var Promise = require('bluebird');
 var XML = require('../services/XML');
+
 
 
 module.exports = {
     method: 'POST',
     path: '/changeset/{changeset_id}/upload',
     handler: function(req, res) {
-        res(200);
+        var changesetID = req.params.changeset_id;
+        if (!changesetID || isNaN(changesetID)) {
+            res(Boom.badRequest('Changeset ID must be a non-zero number'));
+        }
+
+        // Look for changeset in database
+
+        // If it exists
+        var actions = XML.readChanges(req.payload.xmlString);
+        console.log(actions);
+        res(200)
     }
 }
