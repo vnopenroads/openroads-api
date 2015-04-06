cce797f5d7c40fe1ac2b1fd47c00db7766aef52c
Derek Lieu <dl@dereklieu.com>
2015-04-06 14:57:10 -0400

    Test indents

--- test/unit/changesetUpload.test.js
+++ test/unit/changesetUpload.test.js
@@ -1,31 +1,31 @@
 'use strict';
-var mocks = require('./helpers/changesets');
-var knex = require('knex')({ 
-  client: 'pg', 
+var knex = require('knex')({
+  client: 'pg',
   connection: require('../../connection'),
   debug: false
 });
+var mocks = require('./helpers/changesets');
 
 var serverShouldOk = function(mock, done) {
-  var options = { 
+  var options = {
     method: 'POST',
     url: '/changeset/1/upload',
     payload: {
-      'xmlString': mock
+      xmlString: mock
     }
   };
   server.injectThen(options)
-    .then(function(res) {
-      res.statusCode.should.eql(200);
-      return done();
-    }).catch(function(err) {
-      return done(err);
-    });
+  .then(function(res) {
+    res.statusCode.should.eql(200);
+    return done();
+  }).catch(function(err) {
+    return done(err);
+  });
 };
 
 describe('ChangesetsController', function() {
   describe('#upload',function() {
-    it('Creates a node', function(done) { 
+    it('Creates a node', function(done) {
       serverShouldOk(mocks.createNode(-1), done);
     });
 
@@ -36,7 +36,7 @@ describe('ChangesetsController', function() {
       });
     });
 
-    it('Deletes a node', function(done) { 
+    it('Deletes a node', function(done) {
       knex('current_nodes').where('changeset_id', 1)
       .then(function(nodes) {
         serverShouldOk(mocks.deleteNode(nodes[0].id), done);
@@ -52,12 +52,10 @@ describe('ChangesetsController', function() {
       .then(function(nodes) {
         knex('current_ways').where('changeset_id', 1)
         .then(function(ways) {
-          serverShouldOk(mocks.modifyWay(nodes[1].id, 
+          serverShouldOk(mocks.modifyWay(nodes[1].id,
             nodes[2].id, nodes[3].id, ways[0].id), done);
         });
       });
     });
   });
 });
-
-
