503e550453e3257d3bb068030fab246228a548d3
Derek Lieu <dl@dereklieu.com>
2015-04-06 14:33:54 -0400

    Query builder in node, way model files

--- models/Node.js
+++ models/Node.js
@@ -1,22 +1,30 @@
 'use strict';
 /**
-* Nodes.js
-*
-* @description :: Represents nodes.
-* Schema: http://chrisnatali.github.io/osm_notes/osm_schema.html#current_nodes
-*
-*/
+ * Nodes.js
+ *
+ * @description :: Represents nodes.
+ * Schema: http://chrisnatali.github.io/osm_notes/osm_schema.html#current_nodes
+ *
+ */
+
+var _ = require('lodash');
+var Boom = require('boom');
+var Promise = require('bluebird');
+var knex = require('knex')({
+  client: 'pg',
+  connection: require('../connection'),
+  debug: false
+});
+
 var RATIO = require('../services/Ratio');
 var QuadTile = require('../services/QuadTile');
-var knex = require('knex')({ 
-    client: 'pg', 
-    connection: require('../connection'),
-    debug: false
-  });
-var _ = require('lodash');
+var NodeTag = require('./NodeTag');
+var WayNode = require('./WayNode');
+var Way = require('./Way');
 
+module.exports = Node;
 
-module.exports = {
+var Node = {
 
   tableName: 'current_nodes',
 
@@ -83,16 +91,43 @@ module.exports = {
     return model;
   },
 
+  // TODO this function should also handle node#fromJXEntity.
+  // Test and replace to avoid duplication.
+  fromEntity: function(entity, meta) {
+    var ratio = RATIO;
+    var model = {};
+    model.visible = (entity.visible !== 'false' && entity.visible !== false);
+    model.version = parseInt(entity.version, 10) || 1;
+    model.timestamp = new Date();
+    if (entity.lat && entity.lon) {
+      entity.lat = parseFloat(entity.lat);
+      entity.lon = parseFloat(entity.lon);
+      model.latitude = entity.lat * ratio | 0;
+      model.longitude = entity.lon * ratio | 0;
+      model.tile = QuadTile.xy2tile(QuadTile.lon2x(entity.lon), QuadTile.lat2y(entity.lat));
+    }
+    if (entity.id && entity.id > 0) {
+      model.id = entity.id;
+    }
+    if (entity.changeset) {
+      model.changeset_id = parseInt(entity.changeset, 10);
+    }
+    else if (meta && meta.id) {
+      model.changeset_id = parseInt(meta.id);
+    }
+    return model;
+  },
+
   canBeDeleted: function(nodeId) {
     // No need to call parseInt on node_id, as that's already handled upstream.
-    return knex('current_way_nodes')
+    return knex(WayNode.tableName)
     .where('node_id', nodeId)
     .then(function wayNodeResp(wayNodes) {
       // If this node belongs to a way, check to see if
       // any of those ways are visible, aka not deleted yet.
       // Return false if this node is still part of an existing way.
       if (wayNodes) {
-        return knex('current_ways').whereIn('id',_.pluck(wayNodes, 'way_id'))
+        return knex(Way.tableName).whereIn('id',_.pluck(wayNodes, 'way_id'))
         .then(function(ways) {
           var visible = _.chain(ways)
           .pluck('visible')
@@ -131,6 +166,137 @@ module.exports = {
       }
     }
     return entities;
-  }
+  },
+
+  // Generate create, modify, and destroy queries.
+  // Using as few connections as possible.
+  queryGenerator: {
+    create: function(changeset, meta, map) {
+      var creates = changeset.create.node;
+      if (!creates) {
+        return [];
+      }
+
+      // Map each node creation to a model with proper attributes.
+      var models = creates.map(function(entity) {
+        return Node.fromEntity(entity, meta);
+      });
+
+      // Insert all node models, then use returned ids to insert all tags.
+      var query = knex(Node.tableName).insert(models).returning('id').then(function(ids) {
+        var tags = [];
+        for (var i = 0, ii = creates.length; i < ii; ++i) {
+
+          // We need to take the original object from the changeset query here.
+          // It contains node tags.
+          var change = creates[i];
+
+          // Re-map the old node ID to the new node ID on the map object.
+          map.node[change.id] = ids[i];
+
+          // Overwrite old ID's with new Node ID's on the changeset.
+          // This assumes that ID's returned from postgresql maintain the same order
+          // that they are inserted in.
+          change.id = ids[i];
+
+          // Check for Node tags. If they exist, they will be in the form of an array.
+          if (change.tag && change.tag.length) {
+            tags.concat(change.tag.map(function(tag) {
+              return {
+                k: tag.k,
+                v: tag.v,
+                node_id: change.id
+              };
+            }));
+          }
+        }
+
+        // Only save tags if there are any.
+        if (tags.length) {
+          return knex(NodeTag.tableName).insert(tags).catch(function(err) {
+            console.log('err: creating node tags in create');
+            console.log(err);
+          });
+        }
+        else return
+      })
+      .catch(function(err) {
+        console.log('err: inserting new nodes');
+        console.log(err);
+      });
+      return query;
+    },
+
+    modify: function(changeset, meta, map) {
+      var modifies = changeset.modify.node;
+      if (!modifies) {
+        return [];
+      }
+
+      // Create a list of modify queries.
+      // TODO so far as I know, there's no way to bulk-modify a bunch of records.
+      // So set up a list of all of them that we can call Promise.all() on.
+      var nodeChanges = modifies.map(function(entity) {
+
+        // Create a new model object with the proper attributes.
+        var model = Node.fromEntity(entity, meta);
+        var query = knex(Node.tableName).where({id: entity.id}).update(model).catch(function(err) {
+          console.log('err: modify single node');
+          console.log(err);
+        });
+        return query;
+      });
+
+      // We'll need all the Node IDs to delete it's way tags.
+      // Since we're looping over the modify object, record all the
+      // new node tags.
+      var ids = [];
+      var tags = [];
+      for (var i = 0, ii = modifies.length; i < ii; ++i) {
+        var change = modifies[i];
+        ids.push(parseInt(change.id, 10));
+        if (change.tag && change.tag.length) {
+          tags.concat(change.tag.map(function(tag) {
+            return {
+              k: tag.k,
+              v: tag.v,
+              node_id: change.id
+            };
+          }));
+        }
+      }
+
+      // Call Promise.all() on the modifications.
+      // After that's done, we can safely delete node tags and insert new ones.
+      var query = Promise.all(nodeChanges).then(function() {
+        return knex(NodeTag.tableName).whereIn('node_id', ids).del().then(function() {
+          if (tags.length) {
+            return knex(NodeTag.tableName).insert(tags).catch(function(err) {
+              console.log('err: creating node tags in modify');
+              console.log(err);
+            });
+          }
+          else return
+        }).catch(function(err) {
+          console.log('err: deleting node tags in modify.');
+          console.log(err);
+        });
+      }).catch(function(err) {
+        console.log('err: modifying all nodes.');
+        console.log(err);
+      });
+      return query;
+    },
+
+    destroy: function(changeset, meta, map) {
+      var destroys = changeset.delete.node;
+      if (!destroys) {
+        return [];
+      }
+      var ids = _.pluck(destroys, 'id');
+      var query = knex(Node.tableName).whereIn('id', ids).update({ visible: false });
+      return query;
+    }
+  },
 
 };
--- models/Way.js
+++ models/Way.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
 * Way.js
 *
@@ -6,9 +8,22 @@
 *
 */
 
+var _ = require('lodash');
+var Boom = require('boom');
 var Promise = require('bluebird');
+var knex = require('knex')({
+  client: 'pg',
+  connection: require('../connection'),
+  debug: false
+});
 
-module.exports = {
+var Node = require('./Node');
+var WayNode = require('./WayNode');
+var WayTag = require('./WayTag');
+
+module.exports = Way;
+
+var Way = {
   tableName: 'current_ways',
 
   attributes: {
@@ -24,11 +39,7 @@ module.exports = {
       numeric: true,
       primaryKey: true,
       autoIncrement: true,
-      index: true
-    },
-    changeset_id: {
-      type: 'integer',
-      numeric: true,
+      index: true,
       model: 'changesets'
     },
     timestamp: {
@@ -47,7 +58,7 @@ module.exports = {
   },
 
 
-  //Translate the entity from the XML parser into a proper model
+  // Translate the entity from the XML parser into a proper model
   fromJXEntity: function(entity) {
     var model = {
       changeset_id: parseInt(entity.changeset, 10),
@@ -55,6 +66,25 @@ module.exports = {
       version: parseInt(entity.version, 10) || 1,
       visible: (entity.visible !== 'false' && entity.visible !== false),
     };
+    return model;
+  },
+
+  // TODO this function should also handle node#fromJXEntity.
+  // Test and replace to avoid duplication.
+  fromEntity: function(entity, meta) {
+    var model = {};
+    model.visible = (entity.visible !== 'false' && entity.visible !== false);
+    model.version = parseInt(entity.version, 10) || 1;
+    model.timeStamp = new Date();
+    if (entity.id && entity.id > 0) {
+      model.id = entity.id;
+    }
+    if (entity.changeset) {
+      model.changeset_id = parseInt(entity.changeset, 10);
+    }
+    else if (meta && meta.id) {
+      model.changeset_id = parseInt(meta.id);
+    }
     return model;
   },
 
@@ -66,7 +96,7 @@ module.exports = {
   },
 
   attachNodeIDs: function(ways, wayNodes) {
-    // For each way, attach every node it contains using the wayNodes server 
+    // For each way, attach every node it contains using the wayNodes server
     //response.
     for (var j = 0, jj = ways.length; j < jj; ++j) {
       var way = ways[j];
@@ -81,4 +111,168 @@ module.exports = {
     }
     return ways;
   },
+
+  queryGenerator: {
+    create: function(changeset, meta, map) {
+      var creates = changeset.create.way;
+      if (!creates) {
+        return [];
+      }
+
+      // Create a list of models of just way creations with proper attributes.
+      var models = creates.map(function(entity) {
+        return Way.fromEntity(entity, meta);
+      });
+
+      // Bundle all the way insertions, bundle all the way nodes,
+      // then bundle all the tag insertions if any.
+      var query = knex(Way.tableName).insert(models).returning('id').then(function(ids) {
+        var tags = [];
+        var wayNodes = [];
+        for (var i = 0, ii = creates.length; i < ii; ++i) {
+
+          // Using the original change object.
+          // Contains #nd and #tag.
+          var change = creates[i];
+
+          // Map the old way ID to the new ID,
+          // so we can support relations later.
+          map.way[change.id] = ids[i];
+
+          // Set the new ID onto the changeset object.
+          change.id = ids[i];
+
+          // Safe to assume ways will have way nodes.
+          // Use the map#node mapping to get the new Node ids.
+          wayNodes.concat(change.nd.map(function(wayNode, i) {
+            return {
+              way_id: change.id,
+              sequence_id: i,
+              node_id: map.node[wayNode.ref]
+            };
+          }));
+
+          if (change.tag && change.tag.length) {
+            tags.concat(change.tag.map(function(tag) {
+              return {
+                k: tag.k,
+                v: tag.v,
+                way_id: change.id
+              };
+            }));
+          }
+        }
+
+        // The dependents array will always contain a way node insertion query.
+        // If there are way tags, it will include those too.
+        var dependents = [];
+        dependents.push(knex(WayNode.tableName).insert(wayNodes).catch(function(err) {
+          console.log('err: creating way nodes in create');
+          console.log(err);
+        }));
+        if (tags.length) {
+          tags = [].concat.apply([], tags);
+          dependents.push(knex(WayTag.tableName).insert(tags).catch(function(err) {
+            console.log('err: creating way tags in create');
+            console.log(err);
+          }));
+        }
+        return Promise.all(dependents);
+      })
+      .catch(function(err) {
+        console.log('err: inserting new ways');
+        console.log(err);
+      });
+      return query;
+    },
+
+    modify: function(changeset, meta, map) {
+      var modifies = changeset.modify.way;
+      if (!modifies) {
+        return [];
+      }
+
+      // Create a list of modify queries, since we can't do them all in a single query.
+      var wayChanges = modifies.map(function(entity) {
+        var model = Way.fromEntity(entity, meta);
+        var query = knex(Way.tableName).where({ id: entity.id }).update(model).catch(function(err) {
+          console.log('err: modify single way');
+          console.log(err);
+        });
+        return query;
+      });
+
+      // Get all of our new tags and wayNodes.
+      // Save IDs so we can delete old tags and wayNodes.
+      var ids = [];
+      var tags = [];
+      var wayNodes = [];
+      for(var i = 0, ii = modifies.length; i < ii; ++i) {
+        var modify = modifies[i];
+        ids.push(parseInt(modify.id, 10));
+        wayNodes.concat(modify.nd.map(function(wayNode, i) {
+          return {
+            way_id: modify.id,
+            sequence_id: i,
+            node_id: map.node[wayNode.ref]
+          }
+        }));
+        if (modify.tag && modify.tag.length) {
+          tags.concat(modify.tag.map(function(tag) {
+            return {
+              k: tag.k,
+              v: tag.v,
+              way_id: modify.id
+            };
+          }));
+        }
+      }
+
+      var query = Promise.all(wayChanges).then(function() {
+
+        // Dependents is an array where each item is a delete().then.insert().
+        // It will contain way nodes for certain. It will also contain a delete for tags.
+        // If there are new tags, then it will also insert those tags.
+        var dependents = [];
+        dependents.push(knex(WayNode.tableName).whereIn('way_id', ids).del().then(function() {
+          return knex(WayNode.tableName).insert(wayNodes).catch(function(err) {
+            console.log('err: creating way nodes in modify');
+            console.log(err);
+          });
+        })
+        .catch(function(err) {
+          console.log('err: deleting way nodes in modify');
+          console.log(err);
+        }));
+
+        // Again, we always delete old way tags, even if we don't have new ones to add.
+        dependents.push(knex(WayTag.tableName).whereIn('way_id', ids).del().then(function() {
+          if (tags.length) {
+            return knex(WayTag.tableName).insert(tags).catch(function(err) {
+              console.log('err: creating way tags in modify');
+              console.log(err);
+            });
+          }
+          else return
+        })
+        .catch(function(err) {
+          console.log('err: deleting way tags in modify');
+          console.log(err);
+        }));
+        return Promise.all(dependents);
+      });
+
+      return query;
+    },
+
+    destroy: function(changeset, meta, map) {
+      var destroys = changeset.delete.way;
+      if (!destroys) {
+        return [];
+      }
+      var ids = _.pluck(destroys, 'id');
+      var query = knex(Way.tableName).whereI('id', ids).update({ visible: false });
+      return query;
+    }
+  }
 };
