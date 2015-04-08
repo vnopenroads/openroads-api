e294dabcf846815c8ab9c5afb8fe8390a9d339dc
Anand Thakker <vestibule@anandthakker.net>
2015-04-08 13:49:32 -0400

    refactor out toGeoJSON for use in multiple routes

--- routes/map-json.js
+++ routes/map-json.js
@@ -1,14 +1,13 @@
 'use strict';
 var Boom = require('boom');
-var _ = require('lodash');
 var knex = require('knex')({
   client: 'pg',
   connection: require('../connection'),
   debug: false
 });
+var toGeoJSON = require('../services/osm-data-to-geojson.js');
 var queryBbox = require('../services/query-bbox.js');
 var BoundingBox = require('../services/BoundingBox.js');
-var ratio = require('../services/Ratio.js');
 
 module.exports = {
   method: 'GET',
@@ -25,52 +24,7 @@ module.exports = {
     
     queryBbox(knex, bbox)
     .then(function (result) {
-
-      var nodes = result[2];
-      var waytags = result[3];
-      //var nodetags = result[4];
-
-      // attach associated nodes to ways
-      var ways = result[0];
-      ways.forEach(function (way) {
-        way.nodes = result[1].filter(function(waynode) {
-          return waynode.way_id === way.id;
-        });
-        way.tags = waytags.filter(function(tag) {
-          return tag.way_id === way.id;
-        });
-      });
-
-
-      var idToNode = {}; // TODO:this should be a real hashmap
-      nodes.forEach(function (n) { idToNode[n.id] = n; });
-
-      var wayFeatures = ways.map(function (way) {
-        var nodeCoordinates = way.nodes.map(function (waynode) {
-          var node = idToNode[waynode.node_id];
-          return [node.longitude / ratio, node.latitude / ratio];
-        });
-
-        var properties = _.zipObject(way.tags.map(function (t) {
-          return [t.k, t.v];
-        }));
-
-        return {
-          type: 'Feature',
-          properties: properties,
-          geometry: {
-            type: 'LineString',
-            coordinates: nodeCoordinates
-          }
-        };
-      });
-
-      res({
-        type: 'FeatureCollection',
-        properties: {},
-        features: wayFeatures
-      });
-
+      res(toGeoJSON(result[0], result[1], result[2], result[3]));
     })
     .catch(function (err) {
       console.log(err);
---
+++ services/osm-data-to-geojson.js
@@ -0,0 +1,52 @@
+'use strict';
+/* jshint camelcase: false */
+
+var _ = require('lodash');
+var ratio = require('../services/Ratio.js');
+
+/*
+ * Takes the given way/waynode/node models and returns a FeatureCollection
+ * of LineString features--one for each way.
+ */
+module.exports = function toGeoJSON(ways, waynodes, nodes, waytags) {
+
+  // attach associated nodes to ways
+  ways.forEach(function (way) {
+    way.nodes = waynodes.filter(function(waynode) {
+      return waynode.way_id === way.id;
+    });
+    way.tags = waytags.filter(function(tag) {
+      return tag.way_id === way.id;
+    });
+  });
+
+
+  var idToNode = {}; // TODO:this should be a real hashmap
+  nodes.forEach(function (n) { idToNode[n.id] = n; });
+
+  var wayFeatures = ways.map(function (way) {
+    var nodeCoordinates = way.nodes.map(function (waynode) {
+      var node = idToNode[waynode.node_id];
+      return [node.longitude / ratio, node.latitude / ratio];
+    });
+
+    var properties = _.zipObject(way.tags.map(function (t) {
+      return [t.k, t.v];
+    }));
+
+    return {
+      type: 'Feature',
+      properties: properties,
+      geometry: {
+        type: 'LineString',
+        coordinates: nodeCoordinates
+      }
+    };
+  });
+
+  return {
+    type: 'FeatureCollection',
+    properties: {},
+    features: wayFeatures
+  };
+};
