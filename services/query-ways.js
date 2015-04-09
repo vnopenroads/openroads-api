0cfcd16e527ad2970f8037f65f22f43cd0aba8c4
Derek Lieu <dl@dereklieu.com>
2015-04-09 17:14:04 -0400

    Writing relation queries into map query

--- services/query-ways.js
+++ services/query-ways.js
@@ -17,12 +17,19 @@ module.exports = function queryWays(knex, wayIds) {
   }
 
   // Query the desired ways and any way_nodes that are in those ways
+  // Also query any relations that those ways are a part of.
+
+  // TODO this currently does not query nodes that are part of relations,
+  // or other relations that are part of relations.
 
   return Promise.all([
     knex('current_ways').whereIn('id', wayIds),
     knex('current_way_nodes')
       .orderBy('sequence_id', 'asc')
       .whereIn('way_id', wayIds),
+    knex('current_relation_members')
+      .where('member_type', 'Way')
+      .whereIn('member_id', wayIds)
   ])
   .then(function (result) {
 
@@ -34,20 +41,26 @@ module.exports = function queryWays(knex, wayIds) {
       .unique('node_id')
       .pluck('node_id')
       .value();
+    var relationIds = _.pluck(result[2], 'relation_id');
 
     return Promise.all(result.concat([
       select('current_nodes', 'id', nodeIds),
       select('current_way_tags', 'way_id', wayIds),
-      select('current_node_tags', 'node_id', nodeIds)
+      select('current_node_tags', 'node_id', nodeIds),
+      select('current_relations', 'id', relationIds),
+      select('current_relation_tags', 'relation_id', relationIds)
     ]));
   })
   .then(function (resultArr) {
     var result = {
       ways: resultArr[0],
       waynodes: resultArr[1],
-      nodes: resultArr[2],
-      waytags: resultArr[3],
-      nodetags: resultArr[4]
+      members: resultArr[2],
+      nodes: resultArr[3],
+      waytags: resultArr[4],
+      nodetags: resultArr[5],
+      relations: resultArr[6],
+      relationtags: resultArr[7]
     };
 
     // attach associated nodes and tags to ways
@@ -57,6 +70,15 @@ module.exports = function queryWays(knex, wayIds) {
       });
       way.tags = result.waytags.filter(function(tag) {
         return tag.way_id === way.id;
+      });
+    });
+
+    result.relations.forEach(function (relation) {
+      relation.members = result.members.filter(function(member) {
+        return member.relation_id === relation.id;
+      });
+      relation.tags = result.relationtags.filter(function(tag) {
+        return tag.relation_id === relation.id;
       });
     });
 
