bd0c80b504ad330df8313ac82689966413351e50
Anand Thakker <vestibule@anandthakker.net>
2015-04-13 15:44:35 -0400

    [wip] relations list

--- routes/relations.js
+++ routes/relations.js
@@ -1,9 +1,10 @@
 'use strict';
 var Boom = require('boom');
+var _ = require('lodash');
 var knex = require('knex')({
   client: 'pg',
   connection: require('../connection.js'),
-  debug: false
+  debug: true
 });
 var log = require('../services/log.js');
 
@@ -12,17 +13,6 @@ module.exports = {
   path: '/relations',
   handler: function (req, res) {
 
-    function badRequest(err) {
-      log.error('Bad request', err);
-      return res(Boom.badRequest(err));
-    }
-
-    // List
-    var options = {
-      limit: 20,
-      member: null,
-      tag: null
-    };
     var query = req.query;
 
     // Query the relation by it's member iD.
@@ -31,11 +21,48 @@ module.exports = {
     }
 
     // Query the relation by the type of tag it has.
-    // This asks, what projects are status: in progress?
-    else if (query.id) {
+    else {
+      var tagKeys = Object.keys(query);
+      if(tagKeys.length === 0) {
+        return res(Boom.badRequest('Relations endpoint needs member or tags.'));
+      }
 
+      var q = knex('current_relation_tags');
+      tagKeys.forEach(function(key) {
+        q = q.where(function () {
+          this
+            .where('k', key)
+            .andWhere('v', query[key]);
+        });
+      });
+      q.select('relation_id')
+      .then(function (rows) {
+        var ids = _.pluck(rows, 'relation_id');
+        console.log(ids)
+        return queryRelations(ids);
+      })
+      .then(res);
     }
 
   }
 
 };
+
+function queryRelations(relationIds) {
+  return knex('current_relations')
+    .whereIn('id', relationIds)
+    .then(function (rels) {
+      console.log('rels', rels)
+      knex('current_relation_tags')
+      .whereIn('relation_id', relationIds)
+      .then(function (tags) {
+        console.log('tags', tags)
+        rels.forEach(function (rel) {
+          rel.tags = tags.filter(function (t) {
+            return t.relation_id === rel.id;
+          });
+        });
+        return rels;
+      });
+    });
+}
