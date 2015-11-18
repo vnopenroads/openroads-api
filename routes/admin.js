7a718949a5e5cdee2e4bd1517ba00f3dae9391be
Daniel Silva <daniel@developmentseed.org>
2015-11-18 17:12:30 +0000

    Add docs

--- routes/admin.js
+++ routes/admin.js
@@ -199,77 +199,387 @@ module.exports = [
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
   /**
-   * @api {get} /admin/:id Get boundaries and road data of subregions
+   * @api {get} /admin/:id Get metadata about an admin area.
    * @apiGroup Administrative areas
    * @apiName GetAdmin
-   * @apiDescription This endpoint returns the boundaries of the subregions
-   * in the given administrative area, as well as the roads clipped to the region.
+   * @apiDescription This endpoint returns the metadata about the given
+   * admin area.
+   * @apiVersion 0.1.0
+   *
+   * @apiParam {Number} ID of the region, province, municipality, city or 
+   * barangay.
+   * 
+   * @apiSuccess {JSON} metadata
+   *
+   * @apiExample {curl} Example Usage: 
+   *    curl http://localhost:4000/admin/13591204000
+   *
+   * @apiSuccessExample {json} Success-Response:
+   * {
+   *   "id": 13591204000,
+   *   "name": "Dumaran",
+   *   "type": 3,
+   *   "NAME_0": "Philippines",
+   *   "NAME_1": "Region IV-B (Mimaropa)",
+   *   "NAME_2": "Palawan",
+   *   "NAME_3": "Dumaran",
+   *   "ID_1_OR": 13000000000,
+   *   "ID_2_OR": 13590000000,
+   *   "ID_3_OR": 13591204000
+   * }
+   */
+  {
+    method: 'GET',
+    path: '/admin/{id}',
+    handler: function (req, res) {
+      var id = +(req.params.id || '');
+
+      getAdminBoundary(id).then(function (boundary) {
+        var main = fixProperties(boundary, boundary.properties);
+        fixIDs(main).then(function (main) {
+          return res(main);
+        });
+      })
+      .catch(function (err) {
+        console.log('err', err);
+        res(Boom.wrap(err));
+      });
+    }
+  },
+
+  /**
+   * @api {get} /admin/:id/boundary Get metadata and boundaries of an admin area.
+   * @apiGroup Administrative areas
+   * @apiName GetAdminBoundaries
+   * @apiDescription This endpoint returns the metadata and boundaries about the
+   * given admin area.
    * The results are returned in GeoJSON.
    * @apiVersion 0.1.0
    *
-   * @apiParam {Number} id Municipality or Barangay ID.
+   * @apiParam {Number} ID of the region, province, municipality, city or 
+   * barangay.
    * 
-   * @apiSuccess {GeoJSON} subregions List of subregion boundaries
-   * @apiSuccess {GeoJSON} roads  List of roads clipped to subregion
+   * @apiSuccess {GeoJSON} Boundary information
    *
    * @apiExample {curl} Example Usage: 
-   *    curl http://localhost:4000/admin/2110147000
+   *    curl http://localhost:4000/admin/13591204000/boundary
    *
    * @apiSuccessExample {json} Success-Response:
-   *  {
-   *  "subregions": {
-   *    "type": "FeatureCollection",
-   *    "properties": {
-   *      "ID_0": 177,
-   *      "ISO": "PHL",
-   *      "NAME_0": "Philippines",
-   *      "ID_2_OR": 2110000000,
-   *      "NAME_2": "Batanes",
-   *      "ID_3_OR": 2110147000,
-   *      ...
-   *      }
-   *    },
-   *    "features": [{
-   *      "type": "Feature",
-   *      "properties": {
-   *        "ISO": "PHL",
-   *        "NAME_0": "Philippines",
-   *        "NAME_2": "Batanes",
-   *        "NAME_3": "Basco",
-   *        "NAME_4": "Chanarian",
-   *          ...
-   *        "ID_4_OR": 2110147001
-   *      },
-   *      "geometry": {
-   *      "type": "Polygon",
-   *      "coordinates": [[[121.95786285400389, 20.432300567627006],
-   *              ...]]
-   *      },
-   *      "id": "2110147001",
-   *      "name": "Chanarian", 
-   *      },
-   *    ...]
-   *    },
-   *    "roads": {
-   *      "type":"FeatureCollection",
-   *      "properties": {...},
-   *      "features": [...]
-   *    }
-   *  }
-  
+   * {
+   *   "type": "Feature",
+   *   "properties": {
+   *     "id": 13591204000,
+   *     "name": "Dumaran",
+   *     "type": 3,
+   *     "NAME_0": "Philippines",
+   *     "NAME_1": "Region IV-B (Mimaropa)",
+   *     "NAME_2": "Palawan",
+   *     "NAME_3": "Dumaran",
+   *     "ID_1_OR": 13000000000,
+   *     "ID_2_OR": 13590000000,
+   *     "ID_3_OR": 13591204000
+   *   },
+   *   "geometry": {
+   *     "type": "MultiPolygon",
+   *     "coordinates": [...]
+   *   }
+   * }
    */
+  {
+    method: 'GET',
+    path: '/admin/{id}/boundary',
+    handler: function (req, res) {
+      var id = +(req.params.id || '');
 
+      getAdminBoundary(id).then(function (boundary) {
+        var props = fixProperties(boundary, boundary.properties);
+        fixIDs(props).then(function (props) {
+          // Result is a geoJSON;
+          var result = {
+            type: boundary.type,
+            properties: props,
+            geometry: boundary.geometry
+          };
 
+          return res(result);
+        });
+      })
+      .catch(function (err) {
+        console.log('err', err);
+        res(Boom.wrap(err));
+      });
+    }
+  },
 
+  /**
+   * @api {get} /admin/:id/boundary Get metadata and road-network of an admin
+   * area.
+   * @apiGroup Administrative areas
+   * @apiName GetAdminRoadNetwork
+   * @apiDescription This endpoint returns the metadata and road-network about
+   * the given admin area.
+   * The results are returned in GeoJSON.
+   * @apiVersion 0.1.0
+   *
+   * @apiParam {Number} ID municipality, city or barangay.
+   * 
+   * @apiSuccess {GeoJSON} Road network information
+   *
+   * @apiExample {curl} Example Usage: 
+   *    curl http://localhost:4000/admin/13591204000/road-network
+   *
+   * @apiSuccessExample {json} Success-Response:
+   * {
+   *   "type": "FeatureCollection",
+   *   "properties": {
+   *     "id": 13591204000,
+   *     "name": "Dumaran",
+   *     "type": 3,
+   *     "NAME_0": "Philippines",
+   *     "NAME_1": "Region IV-B (Mimaropa)",
+   *     "NAME_2": "Palawan",
+   *     "NAME_3": "Dumaran",
+   *     "ID_1_OR": 13000000000,
+   *     "ID_2_OR": 13590000000,
+   *     "ID_3_OR": 13591204000
+   *   },
+   *   "features": [
+   *     {
+   *       "type": "Feature",
+   *       "properties": {
+   *         "highway": "road",
+   *         "source": "Palawan Provincial Government, in association with REID Foundation and World bank."
+   *       },
+   *       "geometry": {
+   *         "type": "LineString",
+   *         "coordinates": [...]
+   *       }
+   *     }
+   *   ]
+   * }
+   */
+  {
+    method: 'GET',
+    path: '/admin/{id}/road-network',
+    handler: function (req, res) {
+      var id = +(req.params.id || '');
 
+      getAdminBoundary(id).then(function (boundary) {
+        return queryPolygon(boundary).then(function (roads) {
+          var props = fixProperties(boundary, roads.properties);
+          fixIDs(props).then(function (props) {
+            // Result is a geoJSON;
+            var result = {
+              type: roads.type,
+              properties: props,
+              features: roads.features
+            };
+            return res(result);
+          });
+        });
+      })
+      .catch(function (err) {
+        console.log('err', err);
+        res(Boom.wrap(err));
+      });
+    }
+  },
 
+  {
+    method: 'GET',
+    path: '/admin',
+    handler: function (req, res) {
+      return listSubregions().then(function (subregions) {
+        // Fix subregion id.
+        _.forEach(subregions, function (o) {
+          o.id = +(o.id);
+        });
+        return res({ adminAreas: subregions });
+      })
+      .catch(function (err) {
+        console.log('err', err);
+        res(Boom.wrap(err));
+      });
+    }
+  },
 
+  /**
+   * @api {get} /admin/:id/subregions Get metadata of subregions of an admin area.
+   * @apiGroup Administrative areas
+   * @apiName GetAdminSubregions
+   * @apiDescription This endpoint returns the metadata about the subregions of 
+   * a given admin area.
+   * @apiVersion 0.1.0
+   *
+   * @apiParam {Number} ID of the region, province, municipality, city or 
+   * barangay.
+   * 
+   * @apiSuccess {JSON} metadata
+   *
+   * @apiExample {curl} Example Usage: 
+   *    curl http://localhost:4000/admin/13591204000/subregions
+   *
+   * @apiSuccessExample {json} Success-Response:
+   * {
+   *   "id": 13591204000,
+   *   "name": "Dumaran",
+   *   "type": 3,
+   *   "NAME_0": "Philippines",
+   *   "NAME_1": "Region IV-B (Mimaropa)",
+   *   "NAME_2": "Palawan",
+   *   "NAME_3": "Dumaran",
+   *   "ID_1_OR": 13000000000,
+   *   "ID_2_OR": 13590000000,
+   *   "ID_3_OR": 13591204000,
+   *   "adminAreas": [
+   *     {
+   *       "name": "Bacao",
+   *       "id": 13591204001,
+   *       "type": 4
+   *     }
+   *   ]
+   * }
+   */
+  {
+    method: 'GET',
+    path: '/admin/{id}/subregions',
+    handler: function (req, res) {
+      var id = +(req.params.id || '');
 
+      getAdminBoundary(id).then(function (boundary) {
+        return listSubregions(boundary.adminType, id, boundary).then(function (subregions) {
+          // Fix subregion id.
+          _.forEach(subregions, function (o) {
+            o.id = +(o.id);
+          });
 
+          var main = fixProperties(boundary, boundary.properties);
+          fixIDs(main).then(function (main) {
+            main.adminAreas = subregions;
+            return res(main);
+          });
+        });
+      })
+      .catch(function (err) {
+        console.log('err', err);
+        res(Boom.wrap(err));
+      });
+    }
+  },
+  /**
+   * @api {get} /admin/:id/subregions/boundary Get metadata and boundaries of the
+   * subregions of an admin area.
+   * @apiGroup Administrative areas
+   * @apiName GetAdminSubregionsBoundaries
+   * @apiDescription This endpoint returns the metadata and boundaries about the
+   * subregions of a given admin area.
+   * The results are returned in GeoJSON.
+   * @apiVersion 0.1.0
+   *
+   * @apiParam {Number} ID of the municipality, city or barangay.
+   * 
+   * @apiSuccess {GeoJSON} Boundary information
+   *
+   * @apiExample {curl} Example Usage: 
+   *    curl http://localhost:4000/admin/13591204000/subregions/boundary
+   *
+   * @apiSuccessExample {json} Success-Response:
+   * {
+   *   "type": "FeatureCollection",
+   *   "properties": {
+   *     "NAME_0": "Philippines",
+   *     "NAME_1": "Region IV-B (Mimaropa)",
+   *     "NAME_2": "Palawan",
+   *     "NAME_3": "Dumaran",
+   *     "ID_1_OR": 13000000000,
+   *     "ID_2_OR": 13590000000,
+   *     "ID_3_OR": 13591204000,
+   *     "id": 13591204000,
+   *     "type": 3,
+   *     "name": "Dumaran"
+   *   },
+   *   "features": [
+   *     {
+   *       "type": "Feature",
+   *       "properties": {
+   *         "id": 13591204001,
+   *         "name": "Bacao",
+   *         "type": 4
+   *       },
+   *       "geometry": {
+   *         "type": "Polygon",
+   *         "coordinates": [
+   *           [
+   *             [
+   *               119.79149627685547,
+   *               10.508410453796499
+   *             ]
+   *           ]
+   *         ]
+   *       }
+   *     }
+   *   ]
+   * }
+   */
+  {
+    method: 'GET',
+    path: '/admin/{id}/subregions/boundary',
+    handler: function (req, res) {
+      var id = +(req.params.id || '');
 
+      getAdminBoundary(id).then(function (boundary) {
+        if (boundary.adminType <= 2) {
+          return res(Boom.badRequest('Request region is too large.'));
+        }
 
+        return getSubregionFeatures(boundary.adminType, id, boundary).then(function (subregions) {
+          var features = _.map(subregions.features, function (o) {
+            var obj = _.pick(o, 'type', 'geometry');
+            switch(boundary.adminType) {
+              case 3:
+                obj.properties = {
+                  id: +(o.id),
+                  name: o.name,
+                  type: 4
+                };
+              break;
+              // There are no subregions for adminType 4
+            }
+            return obj;
+          });
 
+          var props = fixProperties(boundary, subregions.properties);
+          fixIDs(props).then(function (props) {
+            // Result is a geoJSON;
+            var result = {
+              type: subregions.type,
+              properties: props,
+              features: features
+            };
+
+            return res(result);
+          });
+        });
+      })
+      .catch(function (err) {
+        console.log('err', err);
+        res(Boom.wrap(err));
+      });
+    }
+  },
 
   /**
    * @api {get} /admin/search/:name Search for administrative area by name
@@ -317,210 +627,14 @@ module.exports = [
         .orderBy('name')
         .limit(10)
         .then(function (data) {
+          _.forEach(data, function (o) {
+            o.id = +(o.id);
+          });
           res(data);
         });
     }
-  },
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  {
-    method: 'GET',
-    path: '/admin/{id}',
-    handler: function (req, res) {
-      var id = +(req.params.id || '');
-
-      getAdminBoundary(id).then(function (boundary) {
-        var main = fixProperties(boundary, boundary.properties);
-        fixIDs(main).then(function (main) {
-          return res(main);
-        });
-      })
-      .catch(function (err) {
-        console.log('err', err);
-        res(Boom.wrap(err));
-      });
-    }
-  },
-
-  {
-    method: 'GET',
-    path: '/admin/{id}/boundary',
-    handler: function (req, res) {
-      var id = +(req.params.id || '');
-
-      getAdminBoundary(id).then(function (boundary) {
-        var props = fixProperties(boundary, boundary.properties);
-        fixIDs(props).then(function (props) {
-          // Result is a geoJSON;
-          var result = {
-            type: boundary.type,
-            properties: props,
-            geometry: boundary.geometry
-          };
-
-          return res(result);
-        });
-      })
-      .catch(function (err) {
-        console.log('err', err);
-        res(Boom.wrap(err));
-      });
-    }
-  },
-
-  {
-    method: 'GET',
-    path: '/admin/{id}/road-network',
-    handler: function (req, res) {
-      var id = +(req.params.id || '');
-
-      getAdminBoundary(id).then(function (boundary) {
-        return queryPolygon(boundary).then(function (roads) {
-          var props = fixProperties(boundary, roads.properties);
-          fixIDs(props).then(function (props) {
-            // Result is a geoJSON;
-            var result = {
-              type: roads.type,
-              properties: props,
-              features: roads.features
-            };
-            return res(result);
-          });
-        });
-      })
-      .catch(function (err) {
-        console.log('err', err);
-        res(Boom.wrap(err));
-      });
-    }
-  },
-
-  {
-    method: 'GET',
-    path: '/admin',
-    handler: function (req, res) {
-      return listSubregions().then(function (subregions) {
-        // Fix subregion id.
-        _.forEach(subregions, function (o) {
-          o.id = +(o.id);
-        });
-        return res({ adminAreas: subregions });
-      })
-      .catch(function (err) {
-        console.log('err', err);
-        res(Boom.wrap(err));
-      });
-    }
-  },
-
-  {
-    method: 'GET',
-    path: '/admin/{id}/subregions',
-    handler: function (req, res) {
-      var id = +(req.params.id || '');
-
-      getAdminBoundary(id).then(function (boundary) {
-        return listSubregions(boundary.adminType, id, boundary).then(function (subregions) {
-          // Fix subregion id.
-          _.forEach(subregions, function (o) {
-            o.id = +(o.id);
-          });
-
-          var main = fixProperties(boundary, boundary.properties);
-          fixIDs(main).then(function (main) {
-            main.adminAreas = subregions;
-            return res(main);
-          });
-        });
-      })
-      .catch(function (err) {
-        console.log('err', err);
-        res(Boom.wrap(err));
-      });
-    }
-  },
-
-  {
-  method: 'GET',
-  path: '/admin/{id}/subregions/boundary',
-  handler: function (req, res) {
-    var id = +(req.params.id || '');
-
-    getAdminBoundary(id).then(function (boundary) {
-      if (boundary.adminType <= 2) {
-        return res(Boom.badRequest('Request region is too large.'));
-      }
-
-      return getSubregionFeatures(boundary.adminType, id, boundary).then(function (subregions) {
-        var features = _.map(subregions.features, function (o) {
-          var obj = _.pick(o, 'type', 'geometry');
-          switch(boundary.adminType) {
-            case 3:
-              obj.properties = {
-                id: +(o.id),
-                name: o.name,
-                type: 4
-              };
-            break;
-            // There are no subregions for adminType 4
-          }
-          return obj;
-        });
-
-        var props = fixProperties(boundary, subregions.properties);
-        fixIDs(props).then(function (props) {
-          // Result is a geoJSON;
-          var result = {
-            type: subregions.type,
-            properties: props,
-            features: features
-          };
-
-          return res(result);
-        });
-      });
-    })
-    .catch(function (err) {
-      console.log('err', err);
-      res(Boom.wrap(err));
-    });
   }
-}
 ];
-
 
 
 
